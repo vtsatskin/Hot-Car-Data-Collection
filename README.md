f33cb13bf82e1b57a35a6faac01376c852c9da67
Valentin Tsatskin <val@valtsatskin.com>
2016-03-12 14:55:10 -0500

    init session recorder

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Hot-Car-Data-Collection
 A set of scripts to help collect data for our Third Year design project
+
+# Requirements
+
+* Python 3
---
+++ session_recorder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print ('What type of subject is this?')
+print ('\t 1: Person')
+print ('\t 2: Child')
+print ('\t 3: Pet')
+subject_type = input('Enter subject type number [1]: ')
+
+if subject_type
+    subject_type = int(subject_type)
+else
+    subject_type = 1
