3961ae8a9eacaaf2634f36aec254a72282777f00
Valentin Tsatskin <val@valtsatskin.com>
2016-03-19 14:25:26 -0400

    save images to folder

--- session_recorder.py
+++ session_recorder.py
@@ -62,7 +62,7 @@ def collect_sample(sc,conn,cursor,session_id,subject_type,car_id):
 
     with Lepton() as l:
         frame,_ = l.capture()
-        io.imsave('image_data/{}', image_name, frame)
+        io.imsave('image_data/{}'.format(image_name), frame)
 
     _, temperature = Adafruit_DHT.read(sensor, pin)
 
